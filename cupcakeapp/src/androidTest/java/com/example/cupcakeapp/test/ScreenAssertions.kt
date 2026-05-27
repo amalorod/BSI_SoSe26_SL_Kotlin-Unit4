3d821badc9f3e7c40aa8d61569e0b625a7af1275
amalorod <alicmalorodow@gmail.com>
2026-05-27 10:56:15 +0200

    Cupcake App - angepasst

---
+++ cupcakeapp/src/androidTest/java/com/example/cupcakeapp/test/ScreenAssertions.kt
@@ -0,0 +1,12 @@
+
+
+package com.example.cupcakeapp.test
+
+import androidx.navigation.NavController
+import org.junit.Assert
+
+
+
+fun NavController.assertCurrentRouteName(expectedRouteName: String) {
+    Assert.assertEquals(expectedRouteName, currentBackStackEntry?.destination?.route)
+}
